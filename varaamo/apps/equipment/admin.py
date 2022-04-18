5dc5d239afb15a150cdee9531b1c39e776ffa298
Mikko Vilpponen <vilpponen@protecomp.fi>
2022-04-18 14:13:30 +0300

    Better equipment admin

--- varaamo/apps/equipment/admin.py
+++ varaamo/apps/equipment/admin.py
@@ -3,5 +3,21 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.EquipmentClass)
-admin.site.register(models.EquipmentItem)
+class EquipmentClassAdmin(admin.ModelAdmin):
+    list_display = ('name', 'visible', 'rentable')
+
+
+class EquipmentGroupAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+class EquipmentItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'equipment_class_name', 'count', 'equipment_group', 'visible', 'rentable')
+
+    def equipment_class_name(self, equipment):
+        return equipment.equipment_class.name
+
+
+admin.site.register(models.EquipmentClass, EquipmentClassAdmin)
+admin.site.register(models.EquipmentGroup, EquipmentGroupAdmin)
+admin.site.register(models.EquipmentItem, EquipmentItemAdmin)
