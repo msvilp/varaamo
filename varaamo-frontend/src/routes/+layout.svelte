15fb6653041b315906aba51f8cd7be5140154985
Ilmari Markkanen <contact.ilmarimarkkanen@gmail.com>
2024-04-16 09:08:40 +0000

    Remove Discord, Github, Twitter links from navbar and add page for Vuokraus

--- varaamo-frontend/src/routes/+layout.svelte
+++ varaamo-frontend/src/routes/+layout.svelte
@@ -18,11 +18,6 @@
 	.list-nav .title {
 		margin-bottom: 1rem; /* Space between title and buttons */
 	}
-
-	/* Add space between buttons */
-	.list-nav .btn-spaced {
-		margin-bottom: 0.5rem; /* Adjust the value as needed */
-	}
 </style>
 
 <!-- App Shell -->
@@ -31,13 +26,10 @@
 		<!-- App Bar -->
 		<nav class="list-nav">
 			<div class="title">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase">Varaamo</strong>
 			</div>
 			<nav class="list-nav">
-				<a class="btn btn-sm btn-ghost-surface btn-spaced" href="https://discord.gg/EXqV7W8MtY" target="_blank" rel="noreferrer">Discord</a>
-				<a class="btn btn-sm btn-ghost-surface btn-spaced" href="https://twitter.com/SkeletonUI" target="_blank" rel="noreferrer">Twitter</a>
-				<a class="btn btn-sm btn-ghost-surface btn-spaced" href="https://github.com/skeletonlabs/skeleton" target="_blank" rel="noreferrer">GitHub</a>
-				<a href="/" target="_blank" rel="noreferrer">Vuokraus</a>
+				<a href="/vuokraus/" target="_blank" rel="noreferrer">Vuokraus</a>
 			</nav>
 		</nav>
 	</svelte:fragment>
