e452476b52894409e31723740d5e533d8998a8cb
Ilmari Markkanen <contact.ilmarimarkkanen@gmail.com>
2024-04-30 11:03:17 +0000

    Add table to Vuokraus page

--- varaamo-frontend/src/routes/vuokraus/+page.svelte
+++ varaamo-frontend/src/routes/vuokraus/+page.svelte
@@ -1,1 +1,25 @@
+<script lang="ts">
+    import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+    import type { TableSource } from '@skeletonlabs/skeleton';
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+
+    const filteredData = data.feed.filter(equipment => equipment.visible === true);
+
+    const sourceData = filteredData.map(equipment => ({
+        name: equipment.name,
+        count: equipment.count,
+        rentable: equipment.rentable
+    }));
+
+    const tableSimple: TableSource = {
+        head: ["Name", "Count", "Rentable"],
+        body: tableMapperValues(sourceData, ["name", "count", "rentable"]),
+        meta: tableMapperValues(sourceData, ["name", "count", "rentable"]),
+    }
+</script>
+
 <h2>Vuokrattavat tuotteet</h2>
+
+<Table source={tableSimple} />
