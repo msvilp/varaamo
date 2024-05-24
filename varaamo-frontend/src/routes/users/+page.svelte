8a1cf42e586db338f9be7f73e737e999cf966f75
Ilmari Markkanen <contact.ilmarimarkkanen@gmail.com>
2024-05-24 10:13:50 +0000

    Add käyttäjät page and add list of käyttäjät (err with list)

---
+++ varaamo-frontend/src/routes/users/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { PageData } from './$types';
+
+    export let users;
+</script>
+
+<ul>
+    {#each users as user}
+        <li>{user.name}, {user.group}</li>
+    {/each}
+</ul>
