3fb8e71237203615f40a1acb2fac639a1c7fdf33
Mikko Vilpponen <vilpponen@protecomp.fi>
2024-05-24 07:33:22 +0000

    Remove models from Django admin

--- myapps/users/admin.py
+++ myapps/users/admin.py
@@ -2,15 +2,3 @@ from csv import list_dialects
 from django.contrib import admin
 
 from . import models
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "group_name", "admin")
-
-    def group_name(self, model):
-        return model.group.name
-
-class GroupAdmin(admin.ModelAdmin):
-    list_display = ("name", )
-
-admin.site.register(models.RentalUser, UserAdmin)
-admin.site.register(models.RentalUserGroup, GroupAdmin)
